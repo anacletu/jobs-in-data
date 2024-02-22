d9d60e7cb80922fe4d5fc9a629d5138a4b0f962c
Eric <eric.anacleto@gmail.com>
2024-02-22 20:51:15 +0100

    Refactoring functions

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import csv
 import locale
-import pandas as pd
 import os
 import platform
-from forex_python.converter import CurrencyRates
 import requests
+
+import pandas as pd
 from tabulate import tabulate
+from forex_python.converter import CurrencyRates
+
 
 locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
 c = CurrencyRates()
@@ -13,21 +15,21 @@ c = CurrencyRates()
 
 def main():
     df = get_data_pd()
-    data_obj = treat_axis(
-        df
-    ) 
+    data_obj = treat_axis(df)
     removed = False  # boolean to control if the outliers have been removed or not
 
     while True:
-        if (platform.system() == "Windows"):
+        if platform.system() == "Windows":
             os.system("cls")
         else:
-            os.system("clear") # clear the terminal screen after each action from the user, making the visualization more pleasant
+            os.system(
+                "clear"
+            )  # clear the terminal screen after each action from the user, making the visualization more pleasant
 
         action_to_perform = get_user_input()
         match action_to_perform:
             case 1:
-                get_total_lines(removed)
+                get_total_lines(data_obj, removed)
             case 2:
                 get_average_salary(data_obj)
             case 3:
@@ -42,14 +44,15 @@ def main():
                 get_country_summary(data_obj, country)
             case 7:
                 if not removed:
+                    z_score_default = 3
                     z_score_from_user = input(
-                        "Inform the desired threshold for the Z-score method (default is 3): "
+                        f"Inform the desired threshold for the Z-score method (default is {z_score_default}): "
                     )
                     try:
                         z_score_from_user = float(z_score_from_user)
                     except ValueError:
                         print("Invalid input. Using default value.")
-                        z_score_from_user = 3
+                        z_score_from_user = z_score_default
                     rmv_outliers, outliers = detect_outliers(
                         data_obj, z_score_from_user
                     )
@@ -62,15 +65,18 @@ def main():
             case 8:
                 vanilla_analyzer = VanillaPythonAnalysis("db/jobs_in_data.csv")
                 vanilla_analyzer.get_insights()
+            case 9:
+                removed, data_obj = restore_dateset(removed, data_obj, df)
             case _:
                 break
         input("\nPress Enter to continue...\n")
 
 
-def get_total_lines(removed=False):
+def get_total_lines(data, removed=False):
     """
     This function calls different ways to open a db for demonstration purposes
     and prints the total number of lines in the csv file
+    args: removed (bool) - if the outliers have been removed or not (yet)
     """
 
     if not removed:
@@ -93,8 +99,7 @@ def get_total_lines(removed=False):
 
     else:
         # using Pandas to read the data
-        data_obj = get_data_pd()
-        total_lines = data_obj.shape[0]
+        total_lines = data.shape[0]
 
         print("\n************")
         print(f"Total number of lines: {total_lines} using Pandas")
@@ -158,7 +163,9 @@ def treat_axis(data):
     """
     This function is responsible for making the data more readable
     by renaming and removing some columns from the dataset
+    args: data (DataFrame)
     """
+    new_data = data.copy()
     try:
         remove_cols = [
             "work_year",
@@ -167,10 +174,10 @@ def treat_axis(data):
             "salary",
             "company_size",
         ]
-        data.drop(columns = remove_cols, inplace=True)
+        new_data.drop(columns=remove_cols, inplace=True)
 
-        data.rename(
-            columns = {
+        new_data.rename(
+            columns={
                 "job_category": "Job category",
                 "employee_residence": "Country",
                 "experience_level": "Experience level",
@@ -185,7 +192,7 @@ def treat_axis(data):
         print("Failed to treat the data.")
         return None
     else:
-        return data
+        return new_data
 
 
 def get_user_input():
@@ -201,17 +208,20 @@ def get_user_input():
         "6. Type a country to see a summary of the data",
         "7. Detect outliers using Z-score method",
         "8. Check some general insights from the data",
-        "9. Exit program",
+        "9. Restore the original data with the outliers",
+        "0. Exit program",
     ]
     options = "\n".join(options_for_the_user)
-    user_action = input(f"What would you like to do? Type the corresponding number: \n\n{options}\n")
+    user_action = input(
+        f"What would you like to do? Type the corresponding number: \n\n{options}\n"
+    )
     try:
         action = int(user_action)
     except ValueError:
         print("Invalid choice.")
         return get_user_input()
     else:
-        if action in range(1, len(options_for_the_user) + 1):
+        if action in range(0, len(options_for_the_user)):
             return action
         else:
             print("Invalid choice.")
@@ -221,6 +231,7 @@ def get_user_input():
 def get_average_salary(data):
     """
     This function calculates the average salary in USD
+    args: data (DataFrame)
     """
     try:
         average_salary = data["Salary in USD"].mean()
@@ -236,6 +247,7 @@ def get_average_salary_by_country(data):
     """
     This function calculates the average salary by country
     and the percentage variation compared to the global mean salary
+    args: data (DataFrame)
     """
     try:
         overall_mean_salary = data["Salary in USD"].mean()
@@ -246,8 +258,12 @@ def get_average_salary_by_country(data):
     else:
         formatted_data = []
         for country, salary in average_salary_by_country.items():
-            variation_in_percentage = ((salary - overall_mean_salary) / overall_mean_salary) * 100
-            formatted_salary = format_currency(salary)
+            variation_in_percentage = (
+                (salary - overall_mean_salary) / overall_mean_salary
+            ) * 100
+            formatted_salary = format_currency(
+                salary
+            )  # formattig 'on the fly' prevents issues with other calculations
             formatted_data.append(
                 {
                     "Country": country,
@@ -261,9 +277,14 @@ def get_average_salary_by_country(data):
 def group_by_job_category(data):
     """
     This function groups the data by job category
+    args: data (DataFrame)
     """
     try:
-        grouped_data = (data.groupby(["Country", "Job category"])["Salary in USD"].mean().reset_index())
+        grouped_data = (
+            data.groupby(["Country", "Job category"])["Salary in USD"]
+            .mean()
+            .reset_index()
+        )
     except:
         print("Failed to group the data.")
         return
@@ -273,7 +294,7 @@ def group_by_job_category(data):
         )
         print(tabulate(grouped_data, headers="keys", tablefmt="pretty"))
         filter_country = input(
-            "\nIf you want to filter the data by country, type the desired country. Else type Enter.\n"
+            "\nIf you want to isolate one country, specify it. Else press Enter to continue.\n"
         )
         if filter_country:
             filtered_data = grouped_data.loc[grouped_data["Country"] == filter_country]
@@ -287,6 +308,7 @@ def group_by_job_category(data):
 def get_country_info(data, country):
     """
     This function gets the information of a specific country
+    args: data (DataFrame), country (str)
     """
     try:
         country_info = data[data["Country"] == country].copy()
@@ -310,11 +332,13 @@ def get_country_summary(data, country, currency="USD"):
     """
     This function gets the summary of a specific country.
     In addition, it allows the user to convert the currency as requested.
+    It is a long function, but it serves the purpose of showing the data in a more organized way.
+    args: data (DataFrame), country (str), currency (str)
     """
     try:
         country_info = data[
             data["Country"] == country
-        ].copy() # using the copy method in case the user wants to convert the currency
+        ].copy()  # using the copy method in case the user wants to convert the currency (keeping the original data intact)
     except KeyError:
         print("Country not found.")
         return
@@ -343,14 +367,6 @@ def get_country_summary(data, country, currency="USD"):
         lowest_salary = country_info["Salary in USD"].min()
         most_common_emp_type = country_info["Employment type"].mode().values[0]
 
-        local_worker = data[(data["Country"] == country) & (data["Company location"] == country)]
-        salary_if_working_locally = local_worker["Salary in USD"].mean()
-
-        abroad_worker = data[(data["Country"] == country) & (data["Company location"] != country)]
-        salary_if_working_abroad = abroad_worker["Salary in USD"].mean()
-
-        salary_difference = (salary_if_working_abroad - salary_if_working_locally) / salary_if_working_locally * 100
-
         summary = {
             "Country": country,
             "Average Salary": format_currency(average_salary, currency),
@@ -359,14 +375,11 @@ def get_country_summary(data, country, currency="USD"):
             "Highest Salary": format_currency(highest_salary, currency),
             "Lowest Salary": format_currency(lowest_salary, currency),
             "Most Common Employment Type": most_common_emp_type,
-            "Average Salary of Local Workers": format_currency(salary_if_working_locally, currency),
-            "Average Salary of Those Who Work Abroad Remotely": format_currency(salary_if_working_abroad, currency),
-            "Salary Difference": f"{salary_difference:.2f}%",
         }
         print(tabulate([summary], headers="keys", tablefmt="pretty"))
 
         other_currency = input(
-            "\nIf you want to see the information in another currency, type the proper abbreviation (e.g. EUR) else type Enter.\n"
+            "\nIf you want to see the information in another currency, type the proper abbreviation (e.g. EUR) else press Enter.\n"
         )
         if other_currency:
             get_country_summary(data, country, other_currency)
@@ -375,6 +388,7 @@ def get_country_summary(data, country, currency="USD"):
 def convert_currency(base, target, amount):
     """
     This function converts the currency using the forex-python library
+    args: base (str), target (str), amount (float)
     """
     url = "https://api.exchangerate-api.com/v4/latest/" + base
     api_response = requests.get(url)
@@ -385,6 +399,7 @@ def convert_currency(base, target, amount):
 def format_currency(amount, currency="USD"):
     """
     Function to format salary as currency
+    args: amount (float), currency (str)
     """
     if currency == "USD":
         return locale.currency(amount, grouping=True, symbol=True)
@@ -395,13 +410,15 @@ def format_currency(amount, currency="USD"):
 def detect_outliers(data, threshold=3):
     """
     This function checks for outliers in the data using Z-score method.
-    The threshold is set to 3 by default.
+    The threshold is set to 3 by default as per 'empirical rule':
+    https://en.wikipedia.org/wiki/68%E2%80%9395%E2%80%9399.7_rule for more info
+    args: data (DataFrame), threshold (int)
     """
     try:
         z_scores = data["Salary in USD"].apply(
             lambda x: (x - data["Salary in USD"].mean()) / data["Salary in USD"].std()
         )
-        outliers = data[abs(z_scores) > threshold]
+        outliers = data[abs(z_scores) > threshold].copy()
         outliers["Salary in USD"] = outliers["Salary in USD"].apply(format_currency)
     except:
         print("Failed to check for outliers.")
@@ -421,8 +438,16 @@ def remove_outliers(rmv_outliers, data, outliers=None, removed=False):
     """
     This function removes the outliers from the dataset
     as per the user's request
+    args: rmv_outliers (str), data (DataFrame), outliers (DataFrame), removed (bool)
     """
-    if rmv_outliers in ["yes", "y", "yeah", "yep", "sure", "ok"]:
+    if rmv_outliers in [
+        "yes",
+        "y",
+        "yeah",
+        "yep",
+        "sure",
+        "ok",
+    ]:  # trying to catch some misspelling before returning as invalid input
         confirmation = input(
             "\nAre you sure you want to remove the outliers from the dataset? Type 'yes' or 'no': "
         )
@@ -442,12 +467,37 @@ def remove_outliers(rmv_outliers, data, outliers=None, removed=False):
         return removed, data
 
 
+def restore_dateset(removed, data, dataframe):
+    """
+    This function restores the original dataset
+    args: removed (boolean), data (DataFrame), dataframe (DataFrame) - original
+    """
+    if not removed:
+        print("No changes were made to the dataset.")
+    else:
+        ans = input(
+            "Are you sure you want to restore the original data? Type 'yes' to confirm or Enter to continue. "
+        )
+        if ans in ["yes", "y", "yeah", "sure"]:
+            removed = False
+            data = treat_axis(dataframe)
+            print("Original data restored.")
+        else:
+            print("No changes were made to the dataset.")
+
+    return removed, data
+
+
 class VanillaPythonAnalysis:
     def __init__(self, file_path):
         self.data = self.read_data(file_path)
         self.salaries = [float(item.get("salary_in_usd", 0)) for item in self.data]
-        self.job_categories = [item.get("job_category", "Not Available") for item in self.data]
-        self.countries = [item.get("employee_residence", "Not Available") for item in self.data]
+        self.job_categories = [
+            item.get("job_category", "Not Available") for item in self.data
+        ]
+        self.countries = [
+            item.get("employee_residence", "Not Available") for item in self.data
+        ]
         self.years = [int(item.get("work_year", 0)) for item in self.data]
 
     def read_data(self, file_path):
@@ -458,21 +508,21 @@ class VanillaPythonAnalysis:
     def get_average_salary(self):
         average_salary = sum(self.salaries) / len(self.salaries)
         return average_salary
-    
+
     def get_salary_deviaton(self):
         average_salary = self.get_average_salary()
         salary_deviation = [salary - average_salary for salary in self.salaries]
-        deviation_squared = [dev ** 2 for dev in salary_deviation]
+        deviation_squared = [dev**2 for dev in salary_deviation]
         deviation_squared_mean = sum(deviation_squared) / len(deviation_squared)
-        std_dev_salaty = deviation_squared_mean ** 0.5
+        std_dev_salaty = deviation_squared_mean**0.5
         return std_dev_salaty
-    
+
     def get_years_deviaton(self):
         mean_years = sum(self.years) / len(self.years)
         years_deviation = [year - mean_years for year in self.years]
-        deviation_squared = [dev ** 2 for dev in years_deviation]
+        deviation_squared = [dev**2 for dev in years_deviation]
         deviation_squared_mean = sum(deviation_squared) / len(deviation_squared)
-        std_dev_years = deviation_squared_mean ** 0.5
+        std_dev_years = deviation_squared_mean**0.5
         return std_dev_years, mean_years
 
     def get_job_category_frequency(self):
@@ -485,8 +535,11 @@ class VanillaPythonAnalysis:
         mean_salary = self.get_average_salary()
         salary_deviation = self.get_salary_deviaton()
         work_years_deviation, mean_years = self.get_years_deviaton()
-        
-        covariance = sum((year - mean_years) * (salary - mean_salary) for year, salary in zip(self.years, self.salaries)) / len(self.years)
+
+        covariance = sum(
+            (year - mean_years) * (salary - mean_salary)
+            for year, salary in zip(self.years, self.salaries)
+        ) / len(self.years)
 
         correlation = covariance / (work_years_deviation * salary_deviation)
         return correlation
@@ -504,27 +557,32 @@ class VanillaPythonAnalysis:
     def get_lowest_salary(self):
         lowest_salary = min(self.salaries)
         return lowest_salary
-    
+
     def get_highest_salary(self):
         highest_salary = max(self.salaries)
         return highest_salary
 
     def get_insights(self):
-
         print("\n===================== Salary Analysis =====================\n")
         salary_mean = self.get_average_salary()
         std_dev_salary = self.get_salary_deviaton()
         print(f"Global Salary Mean: ${salary_mean:,.2f}")
         print(f"Salary Standard Deviation: ${std_dev_salary:,.2f}\n")
-        print(f"A standard deviation of ${std_dev_salary:,.2f} is relatively high, indicating a large variation in wages.")
-        print(f"In other words, although the average salary is ${salary_mean:,.2f}, many employees earn significantly more or less than this amount.")
+        print(
+            f"A standard deviation of ${std_dev_salary:,.2f} is relatively high, indicating a large variation in wages."
+        )
+        print(
+            f"In other words, although the average salary is ${salary_mean:,.2f}, many employees earn significantly more or less than this amount."
+        )
         print("\n===========================================================\n")
 
         print("\n==================== Frequency Analysis ====================\n")
         frequency = self.get_job_category_frequency()
         for category, count in frequency:
             print(f"Category: {category}, Count: {count}")
-        print("\nThis analysis shows the count for each job category in the data source.")
+        print(
+            "\nThis analysis shows the count for each job category in the data source."
+        )
         print("This number illustrates how common a job category is.")
         print("\n============================================================\n")
 
@@ -533,9 +591,13 @@ class VanillaPythonAnalysis:
         print(f"Correlation between work years and salary: {correlation:.2f}\n")
         print("Values close to 1 or -1 indicate a strong correlation.")
         if correlation > 0:
-            print("A positive correlation suggests that salary and years tend to move in the same direction.")
+            print(
+                "A positive correlation suggests that salary and years tend to move in the same direction."
+            )
         elif correlation < 0:
-            print("A negative correlation suggests that salary tends to move in the opposite direction of years.")
+            print(
+                "A negative correlation suggests that salary tends to move in the opposite direction of years."
+            )
         print("\n============================================================\n")
 
         print("\n==================== Tendency Over Time ====================\n")
@@ -545,7 +607,7 @@ class VanillaPythonAnalysis:
         print("\nThis analysis shows the overall behavior of the salaries.")
         print("This is an important information to side with the above correlation.")
         print("\n============================================================\n")
-        
+
         print("\n======================= Salary Range =======================\n")
         lowest_salary = self.get_lowest_salary()
         highest_salary = self.get_highest_salary()
